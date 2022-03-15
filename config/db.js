014f663fd2a577a299c17d4bfec2b15a46281dcd
Hoang Van Binh <binhhvFX12117@funix.edu.vn>
2022-03-15 16:49:52 +0700

    7. Connect với DB dùng mongoose

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const config = require('config');
+
+const db = config.get('mongooseURI');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(db, {
+            useNewUrlParser: true,
+        });
+        console.log('Mongoose conneted . . .');
+    } catch (error) {
+        console.log(error.message);
+        // Exit process with failure
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
