a232af5f4895cd12764d42837e840bafd3700b8e
Hoang Van Binh <binhhvFX12117@funix.edu.vn>
2022-03-17 20:45:16 +0700

    26. Lấy tất cả post, từng post, xóa post phải đúng người tạo mới xóa được của họ

--- routes/api/post.js
+++ routes/api/post.js
@@ -8,8 +8,8 @@ const Profile = require('../../models/profile');
 const { validationResult, check } = require('express-validator/check');
 const auth = require('../../middleware/auth');
 
-// @route   POST api/pots
-//  @desc   Create a post
+// @route   POST api/posts
+// @desc    Create a post
 // @access  Private
 router.post(
     '/',
@@ -36,5 +36,65 @@ router.post(
         }
     }
 );
+
+// @route   GET api/posts
+// @desc    GET all posts
+// @access  Private
+router.get('/', auth, async (req, res) => {
+    try {
+        const posts = await Post.find().sort({ date: -1 });
+
+        res.json(posts);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send('Server Errors');
+    }
+});
+
+// @route   GET api/posts/:postId
+// @desc    GET post by ID
+// @access  Private
+router.get('/:id', auth, async (req, res) => {
+    const postId = req.params.id;
+    try {
+        const post = await Post.findById(postId);
+        if (!post) {
+            return res.status(404).json({ msg: 'post not found!' });
+        }
+
+        res.json(post);
+    } catch (error) {
+        console.log(error.message);
+        if (error.kind === 'ObjectId') {
+            return res.status(404).json({ msg: 'post not found!' });
+        }
+        res.status(500).send('Server Errors');
+    }
+});
+
+// @route   DELETE api/posts/:Id
+// @desc    DELETE post
+// @access  Private
+router.delete('/:id', auth, async (req, res) => {
+    const postId = req.params.id;
+    try {
+        const post = await Post.findById(postId);
+        if (!post) {
+            return res.status(404).json({ msg: 'post not found!' });
+        }
+        if (post.user.toString() !== req.user.id.toString()) {
+            return res.status(401).json({ msg: 'User not authorized' });
+        }
+        await post.remove();
+
+        res.json({ msg: 'Post is removed' });
+    } catch (error) {
+        console.log(error.message);
+        if (error.kind === 'ObjectId') {
+            return res.status(404).json({ msg: 'post not found!' });
+        }
+        res.status(500).send('Server Errors');
+    }
+});
 
 module.exports = router;
