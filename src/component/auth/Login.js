6c1438b6a41d74ba81043dc5511104ebda3242ed
Hoang Van Binh <binhhvFX12117@funix.edu.vn>
2022-03-20 16:47:48 +0700

    35. Lấy được dữ liệu từ 2 form, in ra được console

--- src/component/auth/Login.js
+++ src/component/auth/Login.js
@@ -1,36 +1,68 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Login = () => {
+    const [formLogin, setFormLogin] = useState({
+        email: '',
+        password: '',
+    });
+    const { email, password } = formLogin;
+    // Set DATA IN FORM
+    const onChange = (e) => {
+        setFormLogin({
+            ...formLogin,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    // GET DATA FROM Login
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        console.log(formLogin);
+    };
     return (
-        <div>
-            <div class="alert alert-danger">Invalid credentials</div>
-            <h1 class="large text-primary">Sign In</h1>
-            <p class="lead">
-                <i class="fas fa-user"></i> Sign into Your Account
+        <Fragment>
+            {/* <div className="alert alert-danger">Invalid credentials</div> */}
+            <h1 className="large text-primary">Sign In</h1>
+            <p className="lead">
+                <i className="fas fa-user"></i> Sign into Your Account
             </p>
-            <form class="form" action="dashboard.html">
-                <div class="form-group">
+            <form
+                className="form"
+                action="dashboard.html"
+                onSubmit={(e) => onSubmit(e)}
+            >
+                <div className="form-group">
                     <input
                         type="email"
                         placeholder="Email Address"
                         name="email"
+                        value={email}
+                        onChange={(e) => onChange(e)}
                         required
                     />
                 </div>
-                <div class="form-group">
+                <div className="form-group">
                     <input
                         type="password"
                         placeholder="Password"
                         name="password"
+                        value={password}
+                        onChange={(e) => onChange(e)}
+                        minLength="6"
                     />
                 </div>
-                <input type="submit" class="btn btn-primary" value="Login" />
+                <input
+                    type="submit"
+                    className="btn btn-primary"
+                    value="Login"
+                />
             </form>
-            <p class="my-1">
+            <p className="my-1">
                 Don't have an account? <Link to="register">Sign Up</Link>
             </p>
-        </div>
+        </Fragment>
     );
 };
 export default Login;
