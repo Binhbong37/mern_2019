4e585b22fe4bd7659649cf040287032250d405a5
Hoang Van Binh <binhhvFX12117@funix.edu.vn>
2022-03-21 22:14:41 +0700

    Thêm method PUT file server

--- server.js
+++ server.js
@@ -10,6 +10,20 @@ connectDB();
 app.use(express.json({ extended: false }));
 app.use(express.urlencoded({ extended: false }));
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization, x-auth-token'
+    );
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PATCH, DELETE, PUT'
+    );
+
+    next();
+});
+
 app.get('/', (req, res) => {
     res.send('API Running . . . ');
 });
